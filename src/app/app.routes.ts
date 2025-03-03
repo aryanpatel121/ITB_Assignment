55ef0c247d9c5b9241e4b4f14217bb7746a5bdae
aryanpatel121 <aruuu1199800@gmail.com>
2025-03-03 16:02:46 +0530

    Initial commint

---
+++ src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { UserComponent } from './user/user.component';
+
+export const routes: Routes = [
+  { path: 'login', 
+    component: LoginComponent 
+  },
+  { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'user', component: UserComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
